8bd795f0810d5a884a99b95ccbb8ddf652c4f2eb
supachaidev <88824768+supachaidev@users.noreply.github.com>
2022-09-02 22:23:32 +0700

    feat(web): use Inter font

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
