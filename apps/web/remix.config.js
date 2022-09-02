05cbec45a9efa8bb6834bb916bdbe27ea5e09ad1
supachaidev <88824768+supachaidev@users.noreply.github.com>
2022-09-02 19:59:13 +0700

    style: format with prettier

--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -1,9 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "cloudflare-workers",
-  ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget: 'cloudflare-workers',
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-};
+}
--- apps/web/remix.env.d.ts
+++ apps/web/remix.env.d.ts
@@ -3,24 +3,24 @@
 /// <reference types="@cloudflare/workers-types" />
 
 declare var process: {
-  env: { NODE_ENV: "development" | "production" };
-};
+  env: { NODE_ENV: 'development' | 'production' }
+}
 
-declare module "@remix-run/cloudflare" {
+declare module '@remix-run/cloudflare' {
   import type {
     DataFunctionArgs as RemixDataFunctionArgs,
     SessionStorage,
-  } from "@remix-run/cloudflare/dist/index";
-  export * from "@remix-run/cloudflare/dist/index";
+  } from '@remix-run/cloudflare/dist/index'
+  export * from '@remix-run/cloudflare/dist/index'
 
   export interface AppLoadContext extends Record<string, unknown> {
-    env: Env;
-    sessionStorage: SessionStorage;
+    env: Env
+    sessionStorage: SessionStorage
   }
 
   export interface DataFunctionArgs
-    extends Omit<RemixDataFunctionArgs, "context"> {
-    context: AppLoadContext;
+    extends Omit<RemixDataFunctionArgs, 'context'> {
+    context: AppLoadContext
   }
 
   export interface ActionArgs extends DataFunctionArgs {}
