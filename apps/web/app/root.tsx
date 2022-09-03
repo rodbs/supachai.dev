04eb2bf2fa722d7652453908c5e052a5c9e86ed7
supachaidev <88824768+supachaidev@users.noreply.github.com>
2022-09-04 00:26:54 +0700

    feat(web): update meta

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -14,7 +14,8 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Supachai Dev',
+  description: 'Fullstack Web Developer',
   viewport: 'width=device-width,initial-scale=1',
 })
 
