8e3e8cdc23b148cfc908e86c41f14f78491298ce
supachaidev <88824768+supachaidev@users.noreply.github.com>
2022-09-03 10:48:23 +0700

    feat(web): add hover style to tech stack links

--- apps/web/app/routes/index.tsx
+++ apps/web/app/routes/index.tsx
@@ -70,7 +70,7 @@ export default function Index() {
                     target="_blank"
                     rel="noreferrer"
                     title={name}
-                    className="flex w-fit items-center font-bold text-zinc-800 underline dark:text-zinc-500"
+                    className="flex w-fit items-center font-bold text-zinc-800 underline hover:text-zinc-600 dark:text-zinc-500 dark:hover:text-zinc-700"
                   >
                     {iconSrc ? (
                       <img
