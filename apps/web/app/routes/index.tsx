353b6c43b316dcc2ae50b08951db11f1f49890ef
supachaidev <88824768+supachaidev@users.noreply.github.com>
2022-09-03 12:16:48 +0700

    chore(web): make footer link font-medium

--- apps/web/app/routes/index.tsx
+++ apps/web/app/routes/index.tsx
@@ -109,7 +109,7 @@ export default function Index() {
         <footer className="mt-8 mb-16 border-t pt-4 dark:border-zinc-800">
           <a
             href="mailto:contact@supachai.dev"
-            className="flex w-fit items-center text-zinc-700 underline hover:text-zinc-500 dark:text-zinc-600 dark:hover:text-zinc-700"
+            className="flex w-fit items-center font-medium text-zinc-700 underline hover:text-zinc-500 dark:text-zinc-600 dark:hover:text-zinc-700"
           >
             <EnvelopeIcon className="mr-2 h-6 w-6" />
             Send an email to me
